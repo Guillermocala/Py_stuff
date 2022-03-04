5c02892333c80d5c9e6bd7003fcc619cf215e494
guillermo cala <guillermo.cala7@gmail.com>
2022-03-03 23:56:34 -0500

    ejercicio daily

---
+++ practice/validacion_contrasena.py
@@ -0,0 +1,37 @@
+def validador(contrasena):
+    numeros = "1234567890"
+    alfabeto = "abcdefghijklmnopqrstuvwxyz"
+    tieneMinusculas = False
+    tieneMayusculas = False
+    caracterEspecial = False
+    tieneNumeros = False
+    for caracter in contrasena:
+        if caracter in numeros:
+            tieneNumeros = True
+        elif caracter in alfabeto:
+            tieneMinusculas = True
+        elif caracter in alfabeto.upper():
+            tieneMayusculas = True
+        else:
+            caracterEspecial = True
+    if tieneMayusculas and tieneMinusculas and tieneNumeros and caracterEspecial:
+        return True
+    else:
+        return False
+        
+def espacios(contrasena):
+    whiteSpace = " "
+    if whiteSpace in contrasena:
+        return True
+    else:
+        return False
+
+password = input("Ingrese la contrasena: ")
+if len(password) < 8:
+	print("La contrasena debe tener al menos 8 caracteres")
+elif validador(password) == True:
+    print("La contrasena debe tener minusculas, mayusculas, numeros y al menos 1 caracter especial")
+elif espacios(password) == False:
+    print("La contrasena no puede tener espacios en blanco")
+else:
+    print("Contrasena valida!")
--- practice/validacion_nombres_usuario.py
+++ practice/validacion_nombres_usuario.py
@@ -1,24 +1,15 @@
-def alfanumerico(nombre):
-	numeros = "1234567890"
-	for caracter in nombre:
-		if caracter in numeros:
-			return True
-	return False
-def caracteresIncorrectos(nombre):
+def validador(nombre):
     caracteresDisponibles = "1234567890abcdefghijklmnopqrstuvwxyz"
     for caracter in nombre:
         if not caracter in caracteresDisponibles:
             return True
     return False
-        
 nombre = input("Ingrese el nombre del usuario: ")
 if len(nombre) < 6:
 		print("El nombre del usuario debe contener al menos 6 caracteres")
 elif len(nombre) > 12:
 		print("El nombre del usuario no puede contener mas de 12 caracteres")
-elif alfanumerico(nombre) == False:
-    print("El nombre del usuario debe ser alfanumerico")
-elif caracteresIncorrectos(nombre) == True:
-    print("El nombre del usuario solo debe contener letras y numeros")
+elif validador(nombre) == True:
+    print("El nombre del usuario debe ser alfanumerico(solo letras y numeros)")
 else:
     print("Nombre de usuario valido!")
