46f6b01fca4f886ac6ec5585260b616e9a379f4a
guillermo cala <guillermo.cala7@gmail.com>
2022-03-04 00:08:31 -0500

    corregido, tenia sueño xd

--- practice/validacion_contrasena.py
+++ practice/validacion_contrasena.py
@@ -14,7 +14,7 @@ def validador(contrasena):
             tieneMayusculas = True
         else:
             caracterEspecial = True
-    if tieneMayusculas and tieneMinusculas and tieneNumeros and caracterEspecial:
+    if (tieneMayusculas and tieneMinusculas and tieneNumeros and caracterEspecial):
         return True
     else:
         return False
@@ -29,9 +29,9 @@ def espacios(contrasena):
 password = input("Ingrese la contrasena: ")
 if len(password) < 8:
 	print("La contrasena debe tener al menos 8 caracteres")
-elif validador(password) == True:
+elif espacios(password) == True:
+    print("La contrasena no puede tener espacios en blanco")    
+elif validador(password) == False:
     print("La contrasena debe tener minusculas, mayusculas, numeros y al menos 1 caracter especial")
-elif espacios(password) == False:
-    print("La contrasena no puede tener espacios en blanco")
 else:
     print("Contrasena valida!")
