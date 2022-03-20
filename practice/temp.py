e090cfab34b73c2af11b489d89133b9cd81bc1fd
guillermo cala <guillermo.cala7@gmail.com>
2022-03-20 16:39:22 -0500

    esto es un mensaje de prueba desde vim Please enter the commit message for your changes. Lines starting

--- practice/temp.py
+++ practice/temp.py
@@ -10,3 +10,4 @@ parrot(voltage=1000000, action='VOOOOOM')             # 2 keyword arguments
 parrot(action='VOOOOOM', voltage=1000000)             # 2 keyword arguments
 parrot('a million', 'bereft of life', 'jump')         # 3 positional arguments
 parrot('a thousand', state='pushing up the daisies')  # 1 positional, 1 keyword
+print("soy una modificacion de prueba")
