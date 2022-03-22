fe1a6c67ef8b1c721e887ed7f03baf5ee399c7c0
guillermo cala <guillermo.cala7@gmail.com>
2022-03-21 23:48:53 -0500

    terminando miscelanea listas, falta verificar ultimo ejercicio

--- practice/Ejercicios_listas.py
+++ practice/Ejercicios_listas.py
@@ -78,15 +78,14 @@ def voltear_lista(lista):
 print("lista reverse: ", voltear_lista(lista))
 """
 
-"""     Ejercicio 7: elimina ocurrencias(excepto la primera)"""
+"""     Ejercicio 7: elimina ocurrencias(excepto la primera)
 #sacamos el indice de la primera ocurrencia, eliminamos todos y al final
 #insertamos el dato en donde estaba su primera ocurrencia
 def elimina_ocurrencias(lista, dato):
     if lista.count(dato) != 0:
         position = lista.index(dato)
-        for item in lista:
-            if item == dato:
-                lista.remove(item)
+        while lista.count(dato) != 0:
+            lista.remove(dato)
         lista.insert(position, dato)
     else:
         return "dato no encontrado"
@@ -94,19 +93,48 @@ def elimina_ocurrencias(lista, dato):
     
 eliminar = input("Ingresa el dato a eliminar: ")
 print(elimina_ocurrencias(lista, eliminar))
-
-
-"""     Ejercicio 8: operaciones listas
-def elimina_ocurrencias(lista, dato):
-    if lista.count(dato) != 0:
-        position = lista.index(dato)
-        for item in lista:
-            if item == dato:
-                lista.remove(item)
-        lista.insert(position, dato)
-
-print("lista normal: ", lista)
-for item in lista:
-    elimina_ocurrencias(lista, item)
-print("lista sin repetidos: ", lista)
 """
+
+"""     extra: eliminar repetidos de una lista
+def elimina_ocurrencias(lista):
+    for item in lista:
+        if lista.count(item) > 1:
+            position = lista.index(item)
+            while lista.count(item) != 0:
+                lista.remove(item)
+            lista.insert(position, item)
+"""
+
+"""     Ejercicio 8: operaciones listas"""
+def elimina_ocurrencias(lista):
+    for item in lista:
+        if lista.count(item) > 1:
+            position = lista.index(item)
+            while lista.count(item) != 0:
+                lista.remove(item)
+            lista.insert(position, item)
+
+def estadisticas(lista1, lista2):
+    items_ambas_listas = []
+    primera_no_segunda = []
+    segunda_no_primera = []
+    for item in lista1:
+        if item in lista2:
+            items_ambas_listas.append(item)
+        elif item not in lista2:
+            primera_no_segunda.append(item)
+    for item in lista2:
+        if item not in lista1:
+            segunda_no_primera.append(item)
+    print("Ahora las estadisticas...")
+    print("ambas listas: ", items_ambas_listas)
+    print("primera no segunda: ", primera_no_segunda)
+    print("segunda no primera: ", segunda_no_primera)
+
+
+numero2 = int(input("Ingresa el numero de datos: "))
+lista2 = crear_lista(numero2)
+elimina_ocurrencias(lista)
+elimina_ocurrencias(lista2)
+estadisticas(lista, lista2)
+
