595e881377cd70858fc47884031d038ab260a2f3
guillermo cala <guillermo.cala7@gmail.com>
2022-03-24 00:17:32 -0500

    miscelanea terminada, sucesiones comprendidas

--- practice/Ejercicios_listas2.py
+++ practice/Ejercicios_listas2.py
@@ -48,16 +48,37 @@ numero = int(input("Ingrese el numero: "))
 print(f"Primos hasta {numero} : ", primos(numero))
 """
 
-"""     Ejercicio 4: sucesión 1"""
+"""     Ejercicio 4: sucesión 1
 def sucesion1(a, b, u, cantidad):
-    for i in range(1, cantidad + 1):
-        res = 2 ** i + 1
-        print(res)
-
+    lista = [u]
+    for i in range(cantidad):
+        res = a * lista[len(lista) - 1] + b
+        lista.append(res)
+    return lista
 
 print("Cálculo de términos de una sucesión U(n+1)=a.U(n)+b.")
 a = int(input("Ingrese el valor de a: "))
 b = int(input("Ingrese el valor de b: "))
 u = int(input("Ingrese el valor de U(0): "))
 cant = int(input("Digame cuantos términos quiere: "))
-sucesion1(a, b, u, cant)
+print("Los términos de la sucesión son: ", sucesion1(a, b, u, cant))
+"""
+
+"""     Ejercicio 5: sucesión 2
+def sucesion2(u, cantidad):
+    lista = [u]
+    for i in range(cantidad - 1):
+        ultimo_item = lista[len(lista) - 1]
+        if ultimo_item % 2 == 0:
+            res = ultimo_item // 2
+            lista.append(res)
+        else:
+            res = 3 * ultimo_item + 1
+            lista.append(res)
+    return lista
+
+print("Cálculo de términos de la sucesión U(n+1)=3.U(n)+1 si n es impar y U(n)=U(n)/2 si n es par.")
+u = int(input("Ingrese el valor de U(0): "))
+cant = int(input("Digame cuantos términos quiere: "))
+print("Los términos de la sucesión son: ", sucesion2(u, cant))
+"""
