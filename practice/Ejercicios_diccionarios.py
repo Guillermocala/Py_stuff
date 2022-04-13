f057d1b21bfa55c6261ca0fdee4f5e0857fce782
guillermo cala <guillermo.cala7@gmail.com>
2022-04-12 23:55:09 -0500

    ejercicio diario

--- practice/Ejercicios_diccionarios.py
+++ practice/Ejercicios_diccionarios.py
@@ -10,7 +10,7 @@ diccionario = cuadrados(limite)
 print(diccionario)
 """
 
-"""     Ejercicio 2: ocurrencias cadena"""
+"""     Ejercicio 2: ocurrencias cadena
 def ocurrencias(cadena):
     dic_res = {}
     for item in cadena:
@@ -20,3 +20,56 @@ def ocurrencias(cadena):
 cadena = input("Ingrese una cadena: ")
 diccionario = ocurrencias(cadena)
 print(diccionario)
+"""
+
+"""     Ejercicio 3: precios frutas
+precio_frutas = {'Banano':1400, 'Manzana':3000, 'Fresa':2000, 'Kiwi':4000}
+print("Tienda Frutas\n", precio_frutas)
+fruta = input("Ingrese el nombre de la fruta: ")
+cant_vendida = int(input("Ingrese la cantidad vendida: "))
+if fruta in precio_frutas:
+    precio_final = precio_frutas[fruta] * cant_vendida
+    if cant_vendida > 1:
+        print(f"{cant_vendida} {fruta}s tienen un total de: {precio_final}")
+    else:
+        print(f"{cant_vendida} {fruta} tiene un total de: {precio_final}")
+else:
+    print("Fruta no encontrada")
+"""
+
+"""     Ejercicio 4: notas alumnos"""
+def ingresar_alumnos(cant_alumnos):
+    dic_res = {}
+    cont = 0
+    while cont < cant_alumnos:
+        notas = []
+        nombre = input("Nombre alumno: ")
+        if nombre not in dic_res:
+            while True:
+                nota = float(input("Ingrese nota: "))
+                if nota >= 0:
+                    notas.append(nota)
+                else:
+                    break
+            dic_res[nombre] = notas
+            cont += 1
+        else:
+            print("Alumno ya ingresado!")
+    return dic_res
+
+def diccionario_nota_media(diccionario):
+    dic_res = {}
+    for key, values in diccionario.items():
+        cantidad = len(values)
+        suma = 0
+        for item in values:
+            suma += item
+        media = suma/cantidad
+        dic_res[key] = media
+    return dic_res
+
+cant_alumnos = int(input("Ingrese la cantidad de alumnos: "))
+diccionario = ingresar_alumnos(cant_alumnos)
+print(diccionario)
+diccionario_media = diccionario_nota_media(diccionario)
+print(diccionario_media)
