779e495e53636f96ff9d1ee6b0160a9b16843ba9
Guillermo Cala <guillermo.cala7@gmail.com>
2022-07-27 15:05:32 -0500

    ejercicio diario

--- practice/mclibre/Ejercicios_while3.py
+++ practice/mclibre/Ejercicios_while3.py
@@ -20,20 +20,55 @@ while activador:
         activador = False
 """
 
-"""     Ejercicio 3: azar del 1 al 6 con 2 jugadores"""
+"""     Ejercicio 3: azar del 1 al 6 con 2 jugadores
 activador = True
+suma_jugador1 = 0
+suma_jugador2 = 0
 while activador:
     jugador1 = random.randint(1, 6)
     jugador2 = random.randint(1, 6)
+    suma_jugador1 += jugador1
+    suma_jugador2 += jugador2
     print("Jugador 1: ", jugador1, "\nJugador 2: ", jugador2)
-    if jugador1 > jugador2:
-        print("Gana jugador 1")
-    elif jugador2 > jugador1:
-        print("Gana jugador 2")
-    else:
-        print("Empatados")
-    #se puede simplificar la anidación de los 3 casos de la siguiente forma
-    #print("Gana jugador 1" if jugador1 > jugador2 else "Gana jugador 2" if jugador2 > jugador1 else "Empate")
+    print("Suma jugador 1: ", suma_jugador1, "\nSuma jugador 2: ", suma_jugador2)
     keep = input("Si desea terminar el programa pulse intro...")
     if not keep:
         activador = False
+"se puede simplificar la anidación de los 3 casos de la siguiente forma"
+'print("Gana jugador 1" if jugador1 > jugador2 else "Gana jugador 2" if jugador2 > jugador1 else "Empate")'
+if suma_jugador1 > suma_jugador2:
+    print("Gana jugador 1")
+elif suma_jugador2 > suma_jugador1:
+    print("Gana jugador 2")
+else:
+    print("Empate")
+"""
+
+"""     Ejercicio 4: azar del 1 al 6 con 2 jugadores y eleccion de turno"""
+activador = True
+suma_jugador1 = 0
+suma_jugador2 = 0
+while activador:
+    jugador1 = 0
+    jugador2 = 0
+    player1_turn = input("Turno jugador 1(intro para tirar dado): ")
+    if not player1_turn:
+        jugador1 = random.randint(1, 6)
+        suma_jugador1 += jugador1
+    player2_turn = input("Turno jugador 2(intro para tirar dado): ")
+    if not player2_turn:
+        jugador2 = random.randint(1, 6)
+        suma_jugador2 += jugador2
+    print("Jugador 1: ", jugador1, "\nJugador 2: ", jugador2)
+    print("Suma jugador 1: ", suma_jugador1, "\nSuma jugador 2: ", suma_jugador2)
+    keep_playing = input("Si desea terminar el programa pulse intro...")
+    if not keep_playing:
+        activador = False
+"se puede simplificar la anidación de los 3 casos de la siguiente forma"
+'print("Gana jugador 1" if jugador1 > jugador2 else "Gana jugador 2" if jugador2 > jugador1 else "Empate")'
+if suma_jugador1 > suma_jugador2:
+    print("Gana jugador 1")
+elif suma_jugador2 > suma_jugador1:
+    print("Gana jugador 2")
+else:
+    print("Empate")
