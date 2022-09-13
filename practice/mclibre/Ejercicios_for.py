043b08ca08eeaae820490fa4f54c3d56cd763260
Guillermo Cala <guillermo.cala7@gmail.com>
2022-09-13 18:24:16 -0500

    ejercicio diario y empezando a usar vim

--- practice/mclibre/Ejercicios_for.py
+++ practice/mclibre/Ejercicios_for.py
@@ -1,4 +1,4 @@
-"""     Ejercicio 1: pares e impares en rango"""
+"""     Ejercicio 1: pares e impares en rango
 rango_inicial = int(input("Ingrese el valor inicial: "))
 rango_final = int(input("Ingrese el valor final: "))
 for x in range(rango_inicial, (rango_final + 1)):
@@ -6,3 +6,16 @@ for x in range(rango_inicial, (rango_final + 1)):
         print(x, " es par")
     else:
         print(x, " es impar")
+"""
+
+"""     Ejercicio 2: divisores de un numero"""
+numero = int(input("Ingrese un numero mayor que cero: "))
+while numero <= 0:
+    print("Error")
+    numero = int(input("Ingrese un numero mayor que cero: "))
+else:
+    print("Divisores: ")
+    for x in range(1, (numero + 1)):
+        if numero % x == 0:
+            print(x, end=", ")
+
