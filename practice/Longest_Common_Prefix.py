bc2568ceefded2b0cfeab258e8c25bb4dbf0bfe3
Guillermo Cala <35546879+Guillermocala@users.noreply.github.com>
2023-01-20 23:55:23 -0500

    solucionado, pasa todos los testcases

--- practice/Longest_Common_Prefix.py
+++ practice/Longest_Common_Prefix.py
@@ -1,19 +1,35 @@
+# Guillermo Cala
 # from leetcode
-# incomplete, doesnt pass all testcase
+
+# DESCRIPTION: Write a function to find the longest common prefix 
+# string amongst an array of strings.
+# If there is no common prefix, return an empty string "".
 
 """
 :type strs: List[str]
 :rtype: str
 """
-strs = ["flower", "flow", "flight"]
-index = 1
-prefix = ""
-first = strs[0]
-while index < len(strs):
-    temp = ""
-    for x, y in zip(first, strs[index]):
-        if x == y:
-            temp += x
-    prefix = temp
-    index += 1
-print(prefix)
+#strs = ["flower", "flow", "flight"]
+strs = ["aaa","aa","aaa"]
+
+def longestCommonPrefix(strs):
+    index = 1
+    prefix = []
+    first = strs[0]
+    if len(strs) == 1 and len(first) <= 1:
+        return first
+    else:
+        while index < len(strs):
+            temp = ""
+            equal = True
+            for x, y in zip(first, strs[index]):
+                if x == y and equal:
+                    temp += x
+                else:
+                    equal = False
+            print("temp: ", temp)
+            prefix.append(temp)
+            index += 1
+    return min(prefix, key=len)
+
+print(longestCommonPrefix(strs))
